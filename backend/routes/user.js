c81dc27b3f87b83daf5a45fb9c2ac24249a9056e
Hemant Singh <hemant10jan@gmail.com>
2025-01-09 22:43:54 +0530

    First Commit

---
+++ backend/routes/user.js
@@ -0,0 +1,142 @@
+const express=require("express");
+const router=express.Router()
+const z=require("zod");
+const jwt=require("jsonwebtoken")
+const {JWT_SECRET}=require("../config")
+const {User,Account}=require("../db")
+const {authMiddleware}=require("../middleware")
+
+const signupBody=z.object({
+    username:z.string().email(),
+    password:z.string().min(8).max(24),
+    firstname:z.string().min(1),
+    lastname:z.string().min(1)
+})
+
+const signinBody=z.object({
+    username:z.string().email(),
+    password:z.string().min(8).max(24)
+})
+
+router.post("/signup",async (req,res)=>{
+    const {success} = signupBody.safeParse(req.body)
+
+    if(!success){
+        return res.status(411).json({
+            message:"Invalid Input"
+        })
+    }
+
+    const existingUser = await User.findOne({username:req.body.username})
+
+    if(existingUser){
+        return res.status(409).json({
+            messgage:"User already exists"
+        })
+    }
+
+    const user=await User.create({
+        username:req.body.username,
+        password:req.body.password,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname
+    })
+
+    const userId=user._id;
+
+    await Account.create({
+        userId,
+        balance: 1 + Math.random() * 10000
+    })
+
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json({
+        message:"User created successfully",
+        token
+    })
+
+})
+
+router.post("signin",async(req,res)=>{
+    const {success} = signinBody.safeParse(req.body);
+
+    if(!success){
+        return res.status(411).json({
+            message:"Invalid Input"
+        })
+    }
+
+    const UserDetails=await User.findOne({
+        username:req.body.username,
+        password:req.body.password
+    })
+
+    if(UserDetails){
+        const token=jwt.sign({
+            userId:UserDetails._id
+        },JWT_SECRET)
+
+        res.json({
+            token
+        })
+
+        return;
+    }
+
+    return res.status(411).json({
+        message:"Invalid Credentials/Error while logging in"
+    })
+})
+
+const updateBody=z.object({
+    password:z.string().optional(),
+    firstname:z.string().optional(),
+    lastname:z.string().optional()
+})
+
+router.put("/", authMiddleware, async (req,res)=>{
+    const {success}=updateBody.safeParse(req.body)
+
+    if(!success){
+        return res.status(411).json({message:"Invalid Input/Error while updating user"})
+    }
+
+    await User.updateOne({_id:req.userId},req.body)
+
+    res.json("User updated successfully")
+})
+
+
+router.get("/bulk",async (req,res)=>{
+    const filter = req.query.filter || ""
+
+    const users = await User.find({
+        $or:[
+            {
+                firstname:{
+                    $regex:filter
+                }
+            },
+            {
+                lastname:{
+                    $regex:filter
+                }
+            }
+        ]
+    })
+
+    res.json({
+        user:users.map((user)=>(
+           { _id:user.userId,
+            firstname:user.firstname,
+            lastname:user.lastname,
+            username:user.username}
+        ))
+    })
+})
+
+
+module.exports=router
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import SendMoney from "./pages/SendMoney"
+
+
+function App() {
+  
+
+  return (
+    <SendMoney/>
+  )
+}
+
+export default App
